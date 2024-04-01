67adb39e51af9061ba7692df852934aafd097a18
nur1210 <nur950@gmail.com>
2024-04-01 19:24:56 +0200

    Added gRPC to communicate between services

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+use echo::{echo_service_server::EchoService as Service,
+           EchoRequest, EchoResponse,
+           echo_service_server::EchoServiceServer};
+
+pub mod echo {
+    tonic::include_proto!("echo");
+}
+
+#[derive(Debug, Default)]
+pub struct EchoService {}
+
+#[tonic::async_trait]
+impl Service for EchoService {
+    async fn echo(
+        &self,
+        request: Request<EchoRequest>,
+    ) -> Result<Response<EchoResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let response = EchoResponse {
+            message: format!("Echo: {}", request.into_inner().message),
+        };
+
+        Ok(Response::new(response))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let echo_service = EchoService::default();
+
+    Server::builder()
+        .add_service(EchoServiceServer::new(echo_service))
+        .serve(addr)
+        .await?;
+    Ok(())
 }
