4fb000cc885c1784ca21c23ee46abee52c8a2b19
nur1210 <nur950@gmail.com>
2024-05-25 13:49:18 +0200

    Remove idle time from prometheus exporter

--- src/prometheus.rs
+++ src/prometheus.rs
@@ -1,15 +1,9 @@
 use metrics::{describe_counter, describe_histogram};
 use metrics_exporter_prometheus::PrometheusBuilder;
-use metrics_util::MetricKindMask;
-use std::time::Duration;
 
 pub fn start_prometheus_exporter() {
     PrometheusBuilder::new()
         .with_http_listener(([0, 0, 0, 0], 9090))
-        .idle_timeout(
-            MetricKindMask::COUNTER | MetricKindMask::HISTOGRAM | MetricKindMask::GAUGE,
-            Some(Duration::from_secs(10)),
-        )
         .install()
         .expect("failed to install Prometheus recorder");
 
